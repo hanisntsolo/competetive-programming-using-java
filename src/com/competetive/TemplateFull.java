36e45d9989602782a6b958626b875f0e5fdeda1a
dhsingh <ds.pratap1997@hotmail.com>
2022-03-10 23:48:23 +0530

    TemplateFull.java fill addedyes

--- src/com/competetive/TemplateFull.java
+++ src/com/competetive/TemplateFull.java
@@ -1,6 +1,6 @@
 package com.competetive;
 
-public class Main {
+ class Main {
 
     public static void main(String[] args) {
         System.out.println("\0".length());
--- src/com/competetive/cses/NumberSpiral.java
+++ src/com/competetive/cses/NumberSpiral.java
@@ -1,6 +1,5 @@
 package com.competetive.cses;
 
-import jdk.swing.interop.SwingInterOpUtils;
 
 import java.io.DataInputStream;
 import java.io.FileInputStream;
