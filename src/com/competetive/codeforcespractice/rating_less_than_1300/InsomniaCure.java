6a49c39a013a102ef367808be5f90257f2c2dba3
Dhirendra Pratap Singh <36436712+hanisntsolo@users.noreply.github.com>
2022-12-03 00:06:00 +0530

    codeforces code 4

---
+++ src/com/competetive/codeforcespractice/rating_less_than_1300/InsomniaCure.java
@@ -0,0 +1,186 @@
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+//Static imports for less code
+import static java.math.BigInteger.valueOf;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Math.abs;
+import static java.lang.System.out;
+
+//Utilities
+import java.util.Scanner;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.HashMap;
+import java.util.AbstractMap;
+import java.util.Date;
+public class InsomniaCure {
+
+	public static void main(String[] args) throws IOException {
+		Reader rd = new Reader();
+		int k = rd.nextInt();
+		int l = rd.nextInt();
+		int m = rd.nextInt();
+		int n = rd.nextInt();
+		int totalDragons = rd.nextInt();
+		int damagedDragonFork = totalDragons;
+		ArrayList<Integer> list = new ArrayList<>();
+		while(totalDragons > 0) {
+			list.add(1);
+			totalDragons--;
+		}	
+		if(k == 1) {
+			System.out.println(damagedDragonFork);
+		} else {
+			damageDragon(k, list);
+			// System.out.println(list);
+			damageDragon(l, list);
+			// System.out.println(list);
+			damageDragon(m, list);
+			// System.out.println(list);
+			damageDragon(n, list);
+			// System.out.println(list);
+			int damageDragonCount = 0;
+			for(int dragon : list) {
+				if(dragon == 0)
+					damageDragonCount+=1;
+			}
+			System.out.println(damageDragonCount);
+		}
+
+	}
+	public static void damageDragon(int pos, ArrayList<Integer> list) {
+		for(int i = 0; i < list.size() - pos + 1; i+=pos) {
+			int dragonToDamage = i + pos - 1;
+			if(list.get(dragonToDamage) == 1)
+			list.set(dragonToDamage, 0);
+		}
+	}
+
+
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader()
+        {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Reader(String file_name) throws IOException
+        {
+            din = new DataInputStream(
+                new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException
+        {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    if (cnt != 0) {
+                        break;
+                    }
+                    else {
+                        continue;
+                    }
+                }
+                buf[cnt++] = (byte)c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException
+        {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException
+        {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException
+        {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException
+        {
+            bytesRead = din.read(buffer, bufferPointer = 0,
+                                    BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException
+        {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException
+        {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+}
